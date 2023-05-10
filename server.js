ea18fe19a7a89299ba11d4f646a18ed47567efd4
sonarsarbjeet <126738704+sonarsarbjeet@users.noreply.github.com>
2023-05-10 14:20:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const app=require('express')();
+const server=require('http').createServer(app);
+const io=require('socket.io')(server, { cors: { origin: "*", methods: ["GET", "POST"] } })
+let off=0;
+let ans=0;
+let clients=0;
+
+io.on('connection',(socket)=>{clients++;
+    
+console.log('client connected',clients,ans,off)
+socket.broadcast.emit('new','time to offer')
+socket.on('offer',(offer)=>{
+    if(off===0){
+        console.log('offer recieved')
+    socket.broadcast.emit('offer',offer)
+    off++;
+    }
+})
+socket.on('answer',(answer)=>{
+    if(ans===0){
+        console.log('answer recieved')
+    socket.broadcast.emit('answer',answer)
+    ans++;
+    }
+})
+socket.on('disconnect',()=>{clients--;
+    console.log('client disconnected',clients,off,ans)
+    
+    if(clients===0){
+        off=0;
+        ans=0;
+    }
+})
+}) 
+server.listen(3000)
